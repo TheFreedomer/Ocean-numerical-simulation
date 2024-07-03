55990cdd2ead6f8f94f3bfb95fdb965784a0df50
TheFreedomer <92509015+TheFreedomer@users.noreply.github.com>
2024-07-03 20:33:57 +0800

    Update hw0107.py

--- code/hw0107.py
+++ code/hw0107.py
@@ -4,7 +4,7 @@ from ConvectiveDiffusion import CD01, CD02
 
 # # Set model hyperparameters(time_axis, space_axis, K, A)
 time_axis_ = np.arange(0, 50, 0.001)
-time_axis_02 = np.arange(0, 100, 1.5)
+time_axis_02 = np.arange(0, 10, 1.5)
 space_axis_ = np.arange(0, 1, 0.01)
 K_ = 0.01
 U_ = 0.01
@@ -18,8 +18,15 @@ filepath_ = '../fig/hw0107_cd_1d_ftcs_ns.gif'
 cd01.draw_gif(T_ns_, 200, 100, 20, filepath_)
 
 # #
+cd01 = CD01(time_axis_02, space_axis_, K_, U_)
+T_ns_ = cd01.run_numerical_solution()
+# draw gif
+filepath_ = '../fig/hw0107_cd_1d_ftcs_ns_lamda02.gif'
+cd01.draw_gif(T_ns_, 1, 100, 8, filepath_)
+
+# #
 cd02 = CD02(time_axis_02, space_axis_, K_, U_, bc_)
 T_ns_ = cd02.run_numerical_solution()
 # draw gif
-filepath_ = '../fig/hw0107_cd_1d_btcs_ns.gif'
+filepath_ = '../fig/hw0107_cd_1d_btcs_ns_lamda02.gif'
 cd02.draw_gif(T_ns_, 1, 100, 8, filepath_)
